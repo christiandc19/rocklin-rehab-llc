2e331882951046c00506eb700eb8440d3aa9b0dc
Christian Carandang <91305708+christiandc19@users.noreply.github.com>
2023-02-15 14:45:23 -0800

    styling

--- src/routes/TreatmentPage.js
+++ src/routes/TreatmentPage.js
@@ -3,12 +3,15 @@ import React from 'react'
 import Navbar from '../components/navbar/Navbar'
 import Treatment from '../components/treatment/Treatment'
 import Footer from '../components/footer/Footer'
+import TreatmentSection from '../components/treatment/TreatmentSection'
+
 
 const TreatmentPage = () => {
     return (
         <>
            <Navbar /> 
            <Treatment />
+           <TreatmentSection />
            <Footer />
         </>
     )
